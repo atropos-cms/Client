e04375c959026e36f01e1d32ffc0060ae9efe843
Ralph Huwiler <ralph@huwiler.dev>
2019-05-21 09:51:36 +0200

    redesign login screen

--- layouts/guest.vue
+++ layouts/guest.vue
@@ -3,9 +3,7 @@
     <v-content>
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <nuxt />
-          </v-flex>
+          <nuxt />
         </v-layout>
       </v-container>
     </v-content>
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,31 +1,45 @@
 <template>
-  <v-card>
-    <v-form @submit.prevent="onSubmit">
-      <v-card-text>
-        <v-text-field
-          v-model="credentials.username"
-          prepend-icon="person"
-          name="login"
-          label="Login"
-          type="text"
-        />
-        <v-text-field
-          id="password"
-          v-model="credentials.password"
-          prepend-icon="lock"
-          name="password"
-          label="Password"
-          type="password"
-        />
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn color="primary" type="submit">
-          Login
-        </v-btn>
-      </v-card-actions>
-    </v-form>
-  </v-card>
+  <v-flex xs12 sm8 md8>
+    <v-card :elevation="12">
+      <v-layout row  align-center>
+        <v-flex xs12 lg6 class="pa-4">
+          <v-form @submit.prevent="onSubmit">
+            <v-card-text>
+              <v-text-field
+                v-model="credentials.username"
+                prepend-icon="person"
+                name="login"
+                label="Login"
+                type="text"
+              />
+              <v-text-field
+                id="password"
+                v-model="credentials.password"
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                type="password"
+              />
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="primary" type="submit">
+                Login
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-flex>
+        <v-flex xs6 class="hidden-md-and-down grey lighten-3">
+          <v-img
+            :src="require('~/static/svgs/undraw_authentication_fsn5.svg')"
+            aspect-ratio="1"
+            contain
+            class="ma-4"
+          />
+        </v-flex>
+      </v-layout>
+    </v-card>
+  </v-flex>
 </template>
 
 <script lang="ts">
@@ -35,12 +49,11 @@ import gql from 'graphql-tag'
 @Component({
   layout: 'guest'
 })
-
 export default class AuthLogin extends Vue {
   credentials = {
     username: null,
     password: null
-  }
+  };
 
   async onSubmit() {
     const credentials = this.credentials
