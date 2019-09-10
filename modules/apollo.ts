617d33059421874dae71fdffae6f252c25b691a7
Ralph <ralph@huwiler.dev>
2019-09-10 22:39:41 +0200

    testing graphql websocket subscriptions

--- modules/apollo.ts
+++ modules/apollo.ts
@@ -1,5 +1,7 @@
 export default () => {
   return {
-    httpEndpoint: 'https://server.test/graphql'
+    httpEndpoint: 'https://server.test/graphql',
+
+    wsEndpoint: 'wss://127.0.0.1:3030',
   }
 }
