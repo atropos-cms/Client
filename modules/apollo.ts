0b9294610f364d6d164b22161ea927f0a7387113
Ralph Huwiler <ralph@huwiler.dev>
2019-09-16 14:32:56 +0200

    added echo

--- modules/apollo.ts
+++ modules/apollo.ts
@@ -1,7 +1,26 @@
-export default () => {
-  return {
-    httpEndpoint: 'https://server.test/graphql',
+import ApolloClient from 'apollo-client';
+import { BatchHttpLink } from 'apollo-link-batch-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import EchoLink from './apollo-echo-link';
 
-    wsEndpoint: 'wss://127.0.0.1:3030',
-  }
-}
+const cache = new InMemoryCache();
+const batchHttpLink = new BatchHttpLink({
+  uri: 'https://server.test/graphql',
+});
+const echoLink = new EchoLink();
+const apolloClient = new ApolloClient({
+  link: ApolloLink.from([echoLink, batchHttpLink]),
+  cache,
+  defaultOptions: {
+    watchQuery: {
+      errorPolicy: 'all',
+    },
+    query: {
+      errorPolicy: 'all',
+    },
+  },
+});
+
+
+export default () => apolloClient
--- pages/index.vue
+++ pages/index.vue
@@ -28,9 +28,36 @@ export default Vue.extend({
   apollo: {
     me: gql`query {
       me {
+        id
         first_name
       }
-    }`
+    }`,
+
+    $subscribe: {
+      // When a tag is added
+      tagAdded: {
+        query: gql`subscription tags($id: ID!) {
+          userUpdated(id: $id) {
+            id
+            first_name
+          }
+        }`,
+        // Reactive variables
+        variables () {
+          // This works just like regular queries
+          // and will re-subscribe with the right variables
+          // each time the values change
+          return {
+            id: this.me.id
+          }
+        },
+        // Result hook
+        // Don't forget to destructure `data`
+        result ({ data }) {
+          console.log(data.tagAdded)
+        }
+      }
+    }
   }
 })
 </script>
