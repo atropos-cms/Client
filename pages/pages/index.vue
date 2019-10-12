dbdf3fe60732812f6743ea02344752b4dcd20843
Ralph <ralph@huwiler.dev>
2019-10-12 19:15:03 +0200

    added draggable

--- pages/pages/index.vue
+++ pages/pages/index.vue
@@ -24,62 +24,71 @@
 
     <div class="">
       <v-list>
-        <template
-          v-for="page in pages"
+        <draggable
+          v-model="pages"
+          v-bind="dragOptions"
+          @start="drag = true"
+          @end="drag = false"
         >
-          <v-hover
-            :key="page.id"
-            v-slot:default="{ hover }"
-          >
-            <div>
-              <v-list-item
-                @click="editPage(page)"
+          <transition-group type="transition" :name="!drag ? 'flip-list' : null">
+            <template
+              v-for="page in pages"
+            >
+              <v-hover
+                :key="page.id"
+                v-slot:default="{ hover }"
               >
-                <v-list-item-icon>
-                  <v-icon
-                    :class="{ 'grey--text lighten-1': hover }"
-                    color="transparent"
+                <div>
+                  <v-list-item
+                    @click="editPage(page)"
                   >
-                    drag_indicator
-                  </v-icon>
-                </v-list-item-icon>
+                    <v-list-item-icon>
+                      <v-icon
+                        :class="{ 'grey--text lighten-1': hover }"
+                        color="transparent"
+                      >
+                        drag_indicator
+                      </v-icon>
+                    </v-list-item-icon>
 
-                <v-list-item-content>
-                  <v-list-item-title v-text="page.title" />
-                </v-list-item-content>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="page.title" />
+                    </v-list-item-content>
 
-                <v-list-item-icon>
-                  <v-btn
-                    icon
-                    @click.stop="editPage(page)"
-                  >
-                    <v-icon
-                      :class="{ 'grey--text lighten-1': hover }"
-                      color="transparent"
-                    >
-                      edit
-                    </v-icon>
-                  </v-btn>
-                </v-list-item-icon>
-                <v-list-item-icon>
-                  <v-btn
-                    icon
-                    @click.stop="editPage(page)"
-                  >
-                    <v-icon
-                      :class="{ 'grey--text lighten-1': hover }"
-                      color="transparent"
-                    >
-                      delete
-                    </v-icon>
-                  </v-btn>
-                </v-list-item-icon>
-              </v-list-item>
+                    <v-list-item-icon>
+                      <v-btn
+                        icon
+                        @click.stop="editPage(page)"
+                      >
+                        <v-icon
+                          :class="{ 'grey--text lighten-1': hover }"
+                          color="transparent"
+                        >
+                          edit
+                        </v-icon>
+                      </v-btn>
+                    </v-list-item-icon>
+                    <v-list-item-icon>
+                      <v-btn
+                        icon
+                        @click.stop="editPage(page)"
+                      >
+                        <v-icon
+                          :class="{ 'grey--text lighten-1': hover }"
+                          color="transparent"
+                        >
+                          delete
+                        </v-icon>
+                      </v-btn>
+                    </v-list-item-icon>
+                  </v-list-item>
 
-              <v-divider />
-            </div>
-          </v-hover>
-        </template>
+                  <v-divider />
+                </div>
+              </v-hover>
+            </template>
+          </transition-group>
+        </draggable>
       </v-list>
     </div>
   </v-card>
@@ -87,16 +96,33 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import draggable from 'vuedraggable'
 import PAGES from '~/graphql/pages.gql'
 
 export default Vue.extend({
+  components: {
+    draggable
+  },
+
   data: () => ({
-    pages: []
+    pages: [],
+    drag: false
   }),
 
   apollo: {
     pages: {
       query: PAGES
+    }
+  },
+
+  computed: {
+    dragOptions () {
+      return {
+        animation: 200,
+        group: 'description',
+        disabled: false,
+        ghostClass: 'ghost'
+      }
     }
   },
 
@@ -115,4 +141,25 @@ export default Vue.extend({
 .show-btns {
   color: rgba(255, 255, 255, 1) !important;
 }
+
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.no-move {
+  transition: transform 0s;
+}
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+.list-group {
+  min-height: 20px;
+}
+.list-group-item {
+  cursor: move;
+}
+.list-group-item i {
+  cursor: pointer;
+}
+
 </style>
